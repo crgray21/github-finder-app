d07c8464ba5998395470bd2dfcb1d3c14bae2c2d
Gray, Cory (Genworth) <Cory.Gray@genworth.com>
2019-07-22 18:01:12 -0400

    Data fetched after each keydown event

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+const github = new GitHub;
+
 const searchUser = document.getElementById('searchUser')
 
 searchUser.addEventListener('keyup', (e) => {
     const userText = e.target.value;
 
     if (userText !== '') {
-        console.log(userText);
+        github.getUser(userText)
+        .then(data => {
+            if (data.profile.message === 'Not Found') {
+                //Alert here
+            } else {
+                //Show Profile
+            }
+        });
+    } else {
+        // Clear profile
     }
 });
